6e7640c961b4fe72d3305f6b0cfcc29dc33a41f3
RyoKr <115417859+ryokr@users.noreply.github.com>
2024-12-03 23:24:07 +0700

    ✦ Cooked

--- Ryo.cpp
+++ Ryo.cpp
@@ -715,8 +715,8 @@ const PCWSTR WeatherIconWidth = L"MaxWidth=26";
 const PCWSTR SearchBoxDisplayText = L"Text=✦ Meow";
 
 const PCWSTR TaskbarBackgroundTransform = L"Transform3D:=<CompositeTransform3D TranslateX=\"156.5\"/>";
-const PCWSTR TaskbarTransform = L"Transform3D:=<CompositeTransform3D TranslateX=\"-798\"/>";
-const PCWSTR SystemtrayTransform = L"Transform3D:=<CompositeTransform3D TranslateX=\"1126.5\"/>";
+const PCWSTR TaskbarTransform = L"Transform3D:=<CompositeTransform3D TranslateX=\"-790\"/>";            // incr to right
+const PCWSTR SystemtrayTransform = L"Transform3D:=<CompositeTransform3D TranslateX=\"1134.5\"/>";
 
 const Theme themeRyoMeow = {{
     ThemeTargetStyles{L"Taskbar.TaskbarFrame#TaskbarFrame", { L"Height=56", WidthAuto, HorizontalAlignRight, TaskbarTransform }},
@@ -747,7 +747,7 @@ const Theme themeRyoMeow = {{
     // ThemeTargetStyles{L"Windows.UI.Xaml.Controls.TextBlock#InnerTextBlock[Text=\uE971]", { L"Text=\uEC43"}},    // uE759-thoi uEC43-dot uECCB-largerdot uE712-dots
 
     ThemeTargetStyles{L"Windows.UI.Xaml.Shapes.Rectangle#BackgroundStroke", { HideItem }},                      // line in main section
-    ThemeTargetStyles{L"SystemTray.OmniButton#NotificationCenterButton > Grid > ContentPresenter > ItemsPresenter > StackPanel > ContentPresenter > SystemTray.IconView > Grid > Grid > SystemTray.TextIconContent", { HideItem }}, // notification icon
+    // ThemeTargetStyles{L"SystemTray.OmniButton#NotificationCenterButton > Grid > ContentPresenter > ItemsPresenter > StackPanel > ContentPresenter > SystemTray.IconView > Grid > Grid > SystemTray.TextIconContent", { HideItem }}, // notification icon
 }};
 // -------------------------------------------------------------------------------------------------------------
 // clang-format on
